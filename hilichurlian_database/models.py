a9c1ebec8428a0a0f1c2ade755ee947119a79a96
Sarah Meng <shm3px@virginia.edu>
2021-05-28 13:10:56 -0400

    oops, remove extra many to many

--- hilichurlian_database/models.py
+++ hilichurlian_database/models.py
@@ -4,18 +4,17 @@ from django.db import models
 
 # all data will be official data only (stuff that can be found in the game, not speculation from players)
 
+class Word(models.Model):
+	word = models.CharField(max_length=25, primary_key=True)
+	translation = models.CharField(max_length=100, blank=True)
+
 class CompleteUtterance(models.Model):
 	# making the autofield explicit as a reminder
 	id = models.AutoField(primary_key=True)
 	# utterances do not have to be unique!
 	utterance = models.CharField(max_length=200)
-	words = models.ManyToManyField('Word') # auto assign based on utterance
+	words = models.ManyToManyField(Word) # auto assign based on utterance
 	speaker = models.CharField(max_length=75) # db_index=True later?
 	translation = models.CharField(max_length=200, blank=True)
 	context = models.TextField(blank=True)
 	source = models.CharField(max_length=200) # URL
-
-class Word(models.Model):
-	word = models.CharField(max_length=25, primary_key=True)
-	translation = models.CharField(max_length=100, blank=True)
-	utterances = models.ManyToManyField(CompleteUtterance)
