70f2c7787330f6b109de5bf4bd30613c4c1cc410
Sarah Meng <shm3px@virginia.edu>
2021-05-28 04:03:07 -0400

    oops, updating name

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hilichurlian_database.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hilichurlian_database_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
