72588f6ee62174541c37c2e5d2ca84c53df10c57
Sarah Meng <shm3px@virginia.edu>
2021-05-28 04:05:37 -0400

    add setup for pages, starting with index page

---
+++ hilichurlian_database/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'hilichurlian_database'
+urlpatterns = [
+    path('', views.index, name='index'),
+]
--- hilichurlian_database_project/settings.py
+++ hilichurlian_database_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hilichurlian_database.apps.HilichurlianDatabaseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
