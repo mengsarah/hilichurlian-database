b595c3c9c699b9500125175f00a2c9e7ed3a5f56
Sarah Meng <shm3px@virginia.edu>
2021-05-31 00:49:04 -0400

    add models to admin site

--- hilichurlian_database/admin.py
+++ hilichurlian_database/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Word, CompleteUtterance
 
-# Register your models here.
+class CompleteUtteranceAdmin(admin.ModelAdmin):
+	fieldsets = [
+		('Content',				{'fields': ['utterance', 'words']}),
+		('Context',				{'fields': ['speaker', 'translation', 'context', 'source']}),
+	]
+	list_display = ('__str__', 'speaker', 'source')
+
+admin.site.register(Word)
+admin.site.register(CompleteUtterance, CompleteUtteranceAdmin)
