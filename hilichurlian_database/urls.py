cdb891af2e4c3fed10062a1988c3a88ca08cc20a
Sarah Meng <shm3px@virginia.edu>
2021-06-02 02:34:10 -0400

    add form submission skeleton

--- hilichurlian_database/urls.py
+++ hilichurlian_database/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = 'hilichurlian_database'
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('submit', views.data_entry, name='data_entry'),
+	# for POST
+	path('add_data', views.add_data, name='add_data'),
 ]
--- hilichurlian_database/views.py
+++ hilichurlian_database/views.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.forms import modelform_factory
+from .models import CompleteUtterance
+
+### FORM CLASSES ###
+# CompleteUtterance: don't show the words field
+CompleteUtteranceForm = modelform_factory(CompleteUtterance, fields=['utterance', 'speaker', 'translation', 'translation_source', 'context', 'source'])
+
+### VIEWS FOR POST ###
+def add_data(request):
+	if request.method == 'POST':
+		data = request.POST
+	else:
+		messages.error(request, "No data received")
+	return redirect("hilichurlian_database:data_entry")
+
+### VIEWS FOR USERS ###
 
 def index(request):
 	return render(request, "hilichurlian_database/index.html")
+
+# the /submit page
+def data_entry(request):
+	# blank form
+	submit_form = CompleteUtteranceForm()
+	return render(request, "hilichurlian_database/submit.html", {'form': submit_form})
