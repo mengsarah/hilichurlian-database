710c9b628ccd31912edb090d094d55fa6f40e86a
Sarah Meng <shm3px@virginia.edu>
2021-06-03 14:09:38 -0400

    Revert "somehow I missed needing to add django_heroku"
    This reverts commit 7f53c59797fe16d3e943d5adf98877856e65b3b0.

--- hilichurlian_database_project/settings.py
+++ hilichurlian_database_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 import dj_database_url
 from dotenv import load_dotenv
 
@@ -148,7 +147,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
-
-# Django-Heroku
-django_heroku.settings(locals())
