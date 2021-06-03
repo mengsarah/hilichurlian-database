4202b5bb0b8172348c6b6f317cac22515fefd367
Sarah Meng <shm3px@virginia.edu>
2021-06-03 15:31:30 -0400

    I think I found it
    maybe I can finally stop pushing to main after this... we'll see

--- hilichurlian_database_project/settings.py
+++ hilichurlian_database_project/settings.py
@@ -101,7 +101,7 @@ if IS_LOCAL:
 		}
 	}
 else:
-	DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+	DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
