70fc262634c22532491d4905bb3f464143bc7d50
Sarah Meng <shm3px@virginia.edu>
2021-06-03 02:14:36 -0400

    add data submission proper

--- hilichurlian_database/views.py
+++ hilichurlian_database/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.forms import modelform_factory
-from .models import CompleteUtterance
+from .models import CompleteUtterance, Word
+
+import re
 
 ### FORM CLASSES ###
 # CompleteUtterance: don't show the words field
@@ -11,6 +13,22 @@ CompleteUtteranceForm = modelform_factory(CompleteUtterance, fields=['utterance'
 def add_data(request):
 	if request.method == 'POST':
 		data = request.POST
+		new_utterance = CompleteUtterance()
+		new_utterance.utterance = data['utterance']
+		new_utterance.speaker = data['speaker']
+		new_utterance.translation = data['translation']
+		new_utterance.translation_source = data['translation_source']
+		new_utterance.context = data['context']
+		new_utterance.source = data['source']
+		new_utterance.save()
+		# get list of words; luckily, transcribed Hilichurlian is relatively simple
+		# (currently don't have to account for punctuation within words)
+		utterance_words = re.findall(r'\w+', data['utterance'].lower())
+		for utt_word in utterance_words:
+			(word_in_db, created) = Word.objects.get_or_create(word=utt_word)
+			new_utterance.words.add(word_in_db)
+		new_utterance.save()
+		messages.success(request, 'Added "' + data['utterance'] + '"')
 	else:
 		messages.error(request, "No data received")
 	return redirect("hilichurlian_database:data_entry")
