2fb4c6c7cad1030b09c5d39115413817a3c339d9
Sarah Meng <shm3px@virginia.edu>
2021-06-06 04:22:54 -0400

    housekeeping
    whitespacing, comments, method order

--- hilichurlian_database/views.py
+++ hilichurlian_database/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.forms import modelform_factory
 from .models import CompleteUtterance, Word
-
 import re
 
 ### FORM CLASSES ###
@@ -10,6 +9,7 @@ import re
 CompleteUtteranceForm = modelform_factory(CompleteUtterance, fields=['utterance', 'speaker', 'translation', 'translation_source', 'context', 'source'])
 
 ### VIEWS FOR POST ###
+
 def add_data(request):
 	if request.method == 'POST':
 		data = request.POST
@@ -33,10 +33,14 @@ def add_data(request):
 		messages.error(request, "No data received")
 	return redirect("hilichurlian_database:data_entry")
 
+
 ### VIEWS FOR USERS ###
 
 def index(request):
 	return render(request, "hilichurlian_database/index.html", {'db': CompleteUtterance.objects.all()})
+
+def about(request):
+	return render(request, "hilichurlian_database/about.html")
 
 def filter_strict(request):
 	utterances = None
@@ -56,6 +60,3 @@ def data_entry(request):
 	# blank form
 	submit_form = CompleteUtteranceForm()
 	return render(request, "hilichurlian_database/submit.html", {'form': submit_form})
-
-def about(request):
-	return render(request, "hilichurlian_database/about.html")
