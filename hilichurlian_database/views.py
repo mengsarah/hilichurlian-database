ef2c6b5d17e99abf560daa0d6ec6753ba45ba70d
Sarah Meng <shm3px@virginia.edu>
2021-06-07 21:52:12 -0400

    add pagination logic

--- hilichurlian_database/views.py
+++ hilichurlian_database/views.py
@@ -3,10 +3,51 @@ from django.contrib import messages
 from django.forms import modelform_factory
 from .models import CompleteUtterance, Word
 import re
+import math
 
 ### FORM CLASSES ###
 # CompleteUtterance: don't show the words field
 CompleteUtteranceForm = modelform_factory(CompleteUtterance, fields=['utterance', 'speaker', 'translation', 'translation_source', 'context', 'source'])
+
+### GLOBAL CONSTANTS ###
+DEFAULT_PAGE_START = 0
+DEFAULT_PAGE_SIZE = 10
+
+### HELPER FUNCTIONS ###
+
+# returns valid page size
+def get_valid_page_size(size, request):
+	page_size = DEFAULT_PAGE_SIZE
+	try:
+		page_size = int(size)
+	except:
+		messages.error(request, "Invalid page size. Page size must be a plain number.")
+		page_size = DEFAULT_PAGE_SIZE
+	if page_size < 1:
+		page_size = 1
+	return page_size
+
+# returns a dict with keys 'page_start', 'page_size', and 'num_pages'
+def get_pagination(start, size, request):
+	page_size = get_valid_page_size(size, request)
+	num_pages = math.ceil(CompleteUtterance.objects.count() / page_size)
+	# get valid page start
+	page_start = DEFAULT_PAGE_START
+	try:
+		page_start = int(start)
+	except:
+		messages.error(request, "Invalid page start. Page start must be a plain number.")
+		page_start = DEFAULT_PAGE_START
+	if page_start < 0:
+		page_start = DEFAULT_PAGE_START
+	if page_start > num_pages:
+		page_start = num_pages
+	return {
+		'page_start': page_start,
+		'page_size': page_size,
+		'num_pages': num_pages
+	}
+
 
 ### VIEWS FOR POST ###
 
@@ -37,15 +78,45 @@ def add_data(request):
 ### VIEWS FOR USERS ###
 
 def index(request):
-	return render(request, "hilichurlian_database/index.html", {'db': CompleteUtterance.objects.all()})
+	db = CompleteUtterance.objects.all()
+	page_start = DEFAULT_PAGE_START
+	page_size = DEFAULT_PAGE_SIZE
+	num_pages = None # definitely will be updated
+	# pagination
+	if request.method == 'GET' and any(p in request.GET for p in ['page_start', 'page_size']):
+		# any() to catch cases where exactly one is given
+		req = request.GET
+		try:
+			valid_pagination_values = get_pagination(req['page_start'], req['page_size'], request)
+			page_start = get_pagination['page_start']
+			page_size = get_pagination['page_start']
+			num_pages = get_pagination['num_pages']
+		except:
+			messages.error(request, "Something went wrong while trying to view the data you wanted.")
+			page_start = DEFAULT_PAGE_START
+			page_size = DEFAULT_PAGE_SIZE
+		# slice db to "current page"
+		db = db[page_start : page_start + page_size]
+	if num_pages is None:
+		num_pages = math.ceil(CompleteUtterance.objects.count() / page_size)
+	current_page = math.ceil(page_start / page_size) + 1
+	if current_page > num_pages:
+		current_page = num_pages
+	return render(request, "hilichurlian_database/index.html", {
+		'db': db,
+		'page_start': page_start,
+		'page_size': page_size,
+		'num_pages': range(num_pages),
+		'current_page': current_page,
+	})
 
 def about(request):
 	return render(request, "hilichurlian_database/about.html")
 
 def filter_strict(request):
 	utterances = None
-	req = request.GET
-	if request.method == 'GET' and 'search' in req:
+	if request.method == 'GET' and 'search' in request.GET:
+		req = request.GET
 		word = req['search'].strip()
 		if len(word) == 0:
 			utterances = CompleteUtterance.objects.all()
