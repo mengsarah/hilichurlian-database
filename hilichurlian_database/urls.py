f252eb862158b0d47b0235c0a7d8ccf177df781e
Sarah Meng <shm3px@virginia.edu>
2021-06-08 01:18:06 -0400

    add user-facing pagination
    add the URL and add HTML/CSS

--- hilichurlian_database/urls.py
+++ hilichurlian_database/urls.py
@@ -6,6 +6,8 @@ app_name = 'hilichurlian_database'
 # be careful to not overlap with _project/urls.py
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('view', views.index, name='view'),
+	path('view/<int:page>', views.index, name='view'),
 	path('about', views.about, name='about'),
 	path('filter', views.filter_strict, name='filter_strict'),
 	path('submit', views.data_entry, name='data_entry'),
